2c93dfcce1171439f1fc9017f1ac949152671b82
anushka092003 <100448825+anushka092003@users.noreply.github.com>
2022-05-03 23:07:13 -0500

    Create milestone3.py

---
+++ milestone3.py
@@ -0,0 +1,16 @@
+
+def assemble_genome2(dna_list):
+  n = len(dna_list)
+  overlaps = [[0 for _ in range(n)] for _ in range(n)]
+  for i in range(n):
+    for j in range(n):
+      if i == j:
+        continue
+      x, y = dna_list[i], dna_list[j]
+      #size = len(x)
+      for k in range(1, len(x)):
+        if y.startswith(x[k:]): #checking if string 
+          overlaps[i][j] = len(x) - k
+          break
+          
+assemble_genome2(load_file('ms3-dna-mammuthus.txt'))
