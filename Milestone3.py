c57540277adc2ea516eb136684b7c1cfc088fb46
anushka092003 <100448825+anushka092003@users.noreply.github.com>
2022-05-04 19:45:08 -0500

    Update Milestone3.py

--- Milestone3.py
+++ Milestone3.py
@@ -1,31 +1,35 @@
 def assemble_genome2(dna_list):
-    dict = {}
+    dic={}
     for i in range(len(dna_list)):
         for j in range(len(dna_list)):
-            if i != j:
-                y = 0
+            if i!=j:
+                y=0
                 for k in range(8):
                     if dna_list[j][:8]==dna_list[i][-8:]:
-                        y = k
-                dict[(i,j)] = y
-    if max(dict.values()) > 0 :
-        ret = "".join(dna_list)
-        l =len(ret)
-        string = []
-        while string:
-            ans,cur,remain=string.pop()
+                        y=k
+                dic[(i,j)]=y
+    if max(dic.values())>0:
+        ret="".join(dna_list)
+        l=len(ret)
+        stack=[]
+        for i,wd in enumerate(dna_list):
+            tmp=set(range(len(dna_list)))
+            tmp.remove(i)
+            stack.append((wd,i,tmp))
+        while stack:
+            ans,cur,remain=stack.pop()
             if len(ans)<l:
                 if not remain:
-                    ret = ans
+                    ret=ans
                     l=len(ans)
                 else:
-                    tmp=[[dict[cur,idx],idx] for idx in remain]
+                    tmp=[[dic[cur,idx],idx] for idx in remain] # [#overlap,idx]
                     tmp.sort()
                     for y,idx in tmp:
-                        nans = ans+dna_list[idx][y:]
+                        nans=ans+dna_list[idx][y:]
                         nremain=set(remain)
                         nremain.remove(idx)
-                        string.append((nans,idx,nremain))
+                        stack.append((nans,idx,nremain))
         return ret
  
 def load_file(file):
